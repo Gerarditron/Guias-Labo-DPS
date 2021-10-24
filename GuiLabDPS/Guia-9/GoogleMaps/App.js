e4c165f5b5e3e12d1780fe499c96295971c70444
Gerarditron <gerardo.jose2011@yahoo.es>
2021-10-24 17:30:21 -0600

    Guias 9 y 10 completadas

---
+++ GuiLabDPS/Guia-9/GoogleMaps/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import HomeScreen from './HomeScreen';
+import WebScreen from './WebScreen';
+
+const AppNavigator = createStackNavigator({
+  Home:{
+    screen: HomeScreen
+  },
+  Web:{
+    screen: WebScreen
+  }
+}, {initialRouteName:'Home'})
+
+export default createAppContainer(AppNavigator)
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
